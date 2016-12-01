cda3b4014436b9cacc00a657dd4266c4c990448b
Bergmann Anja <anja.bergmann@edu.fh-joanneum.at>
2016-12-01 16:01:08 +0100

    method for sending contact form

--- app/src/main/java/itm/immokapfenberg/Contact.java
+++ app/src/main/java/itm/immokapfenberg/Contact.java
@@ -2,6 +2,8 @@ package itm.immokapfenberg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Contact extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Contact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+    }
+
+    public boolean sendContactForm(View v){
+        Log.i("ASDF","Trying to send contact form");
+        return true;
     }
 }
