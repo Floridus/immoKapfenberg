d855dc9c81fb85452085b0fa05b90d406d942844
Christoph Hofer <Christoph Hofer>
2016-12-01 15:30:36 +0100

    one view activity

--- app/src/main/java/itm/immokapfenberg/MainActivity.java
+++ app/src/main/java/itm/immokapfenberg/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_one_view);
     }
 }
