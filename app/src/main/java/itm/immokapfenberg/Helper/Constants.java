ad1eb4357ba8579df1bae18aa3a2b9a24c79bc89
Floridus <florian_magna@hotmail.com>
2016-12-02 04:53:14 +0100

    Update initiative activity

--- app/src/main/java/itm/immokapfenberg/Helper/Constants.java
+++ app/src/main/java/itm/immokapfenberg/Helper/Constants.java
@@ -4,6 +4,7 @@ package itm.immokapfenberg.helper;
 public class Constants {
     public static final String API_URL = "http://www.raumfuerideen-kapfenberg.at/media/galerie/";
     public static final String LOGO_URL = "http://www.raumfuerideen-kapfenberg.at/media/design/kapfenberg.png";
+    public static final String INITATIVE_URL = "http://www.raumfuerideen-kapfenberg.at/media/initiative/Kapfenberg-deine-stadt.jpg";
 
     public static final String STORAGE_FAVOURITE = "my_favourites_file";
 
--- app/src/main/java/itm/immokapfenberg/activity/InitiativeActivity.java
+++ app/src/main/java/itm/immokapfenberg/activity/InitiativeActivity.java
@@ -1,14 +1,24 @@
 package itm.immokapfenberg.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import itm.immokapfenberg.R;
+import itm.immokapfenberg.helper.Constants;
 
 public class InitiativeActivity extends BaseActivity {
+
+    private ImageView iniativeImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initiative);
+
+        iniativeImg = (ImageView)findViewById(R.id.iniativeImg);
+
+        Picasso.with(getApplicationContext()).load(Constants.INITATIVE_URL).into(iniativeImg);
     }
 }
